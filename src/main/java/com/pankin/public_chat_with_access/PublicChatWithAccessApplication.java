0c4f672b062b53c895824030e5de516541c108bb
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-04-28 22:50:39 +0400

    Initial commit

---
+++ src/main/java/com/pankin/public_chat_with_access/PublicChatWithAccessApplication.java
@@ -0,0 +1,12 @@
+package com.pankin.public_chat_with_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicChatWithAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicChatWithAccessApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pankin/public_chat_with_access/PublicChatWithAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankin.public_chat_with_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicChatWithAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
